83b3f3853063ba952e9063680325d65212a767ca
EmmLSP <aexclusivasp@gmail..com>
2025-11-28 14:19:52 -0300

    curso java basico - exercicio 03

---
+++ eclipse-java-basico/src/com/loiane/javabasico/aula13/labs/Exer03.java
@@ -0,0 +1,21 @@
+package com.loiane.javabasico.aula13.labs;
+
+import java.util.Scanner;
+
+public class Exer03 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Entre com o primeiro numero:");
+		int num1 = scan.nextInt();
+		
+		System.out.println("Entre com o segundo numero:");
+		int num2 = scan.nextInt();
+		
+		int soma = num1 + num2;
+		System.out.println("A soma dos numeros é: " + soma);
+	}
+
+}
