023e75fc3db1b84d381043d2332d5db055f9916b
EmmLSP <aexclusivasp@gmail..com>
2025-11-28 15:20:17 -0300

    curso java basico - exercicio 06

---
+++ eclipse-java-basico/src/com/loiane/javabasico/aula13/labs/Exer06.java
@@ -0,0 +1,22 @@
+package com.loiane.javabasico.aula13.labs;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exer06 {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Entre com o raio do circulo:");
+		double raio = scan.nextDouble();
+		
+		double area = Math.PI * Math.pow(raio, 2);
+		
+		System.out.println("A area do circulo é: " + String.format("%.2f", area) + " unidades");
+	}
+
+}
