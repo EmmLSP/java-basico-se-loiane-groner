2787945017abecced2a30f1c80c057e600b672b7
EmmLSP <aexclusivasp@gmail..com>
2025-11-28 16:51:47 -0300

    curso java basico - exercicio 12

---
+++ eclipse-java-basico/src/com/loiane/javabasico/aula13/labs/Exer12.java
@@ -0,0 +1,22 @@
+package com.loiane.javabasico.aula13.labs;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exer12 {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Entre com a altura: ");
+		double altura = scan.nextDouble();
+		
+		double pesoIdeal = (72.7 * altura) - 58;
+		
+		System.out.println("O peso ideal é: " + String.format("%.2f", pesoIdeal) + "kg");
+	}
+
+}
