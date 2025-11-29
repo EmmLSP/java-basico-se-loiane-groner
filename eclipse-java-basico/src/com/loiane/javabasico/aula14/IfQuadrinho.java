907db45602ff976533deb64806e2afa84fcfa4de
EmmLSP <aexclusivasp@gmail..com>
2025-11-29 10:11:36 -0300

    curso java basico - aula 14

---
+++ eclipse-java-basico/src/com/loiane/javabasico/aula14/IfQuadrinho.java
@@ -0,0 +1,17 @@
+package com.loiane.javabasico.aula14;
+
+public class IfQuadrinho {
+
+	public static void main(String[] args) {
+		
+		boolean temBatata = true;
+		int ovos = 6;
+		
+		if (temBatata) {
+			ovos = 9;
+		}
+		
+		System.out.println("Compre " + ovos + " ovos.");
+	}
+
+}
