5f7b0276545a5b7bae2c57c3e29ffecb6538dd02
EmmLSP <aexclusivasp@gmail..com>
2025-12-05 09:46:01 -0300

    curso java basico - exercicio 33

--- eclipse-java-basico/src/com/loiane/javabasico/aula17/labs/Exer33.java
+++ eclipse-java-basico/src/com/loiane/javabasico/aula17/labs/Exer33.java
@@ -10,14 +10,19 @@ public class Exer33 {
 		
 		// s = 1/1 + 2/3 + 3/5 + 4/7 + 5/9 + ... + n/m
 		
+		double soma = 0;
+		
 		System.out.println("Digite o valor de n:");
 		int n = scan.nextInt();
 		
 		System.out.print("S = ");
 		for (int i = 1, j = 1; i <= n; i++, j+=2) {
 			System.out.print(i + "/" + j + " + ");
+			soma += i / j; 
 		}
 		System.out.println("... + n/m.");
+		
+		System.out.println("soma: " + soma);
 	}
 
 }
