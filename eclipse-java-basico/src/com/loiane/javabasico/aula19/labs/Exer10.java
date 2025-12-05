db981d77b9787468ba1befa1303484340104c034
EmmLSP <aexclusivasp@gmail..com>
2025-12-05 15:17:00 -0300

    curso java basico - exercicio 10

---
+++ eclipse-java-basico/src/com/loiane/javabasico/aula19/labs/Exer10.java
@@ -0,0 +1,39 @@
+package com.loiane.javabasico.aula19.labs;
+
+import java.util.Scanner;
+
+public class Exer10 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int[] vetorA = new int[10];
+		int[] vetorB = new int[vetorA.length];
+		
+		for (int i = 0; i < vetorA.length; i++) {
+			System.out.println("Entre com o valor do vetor A da posicao " + i);
+			vetorA[i] = scan.nextInt();
+			vetorB[i] = vetorA[i] % 2; // modulo -> sempre retorna um inteiro
+		}
+
+		System.out.print("Vetor A = [");
+		for (int i = 0; i < vetorA.length; i++) {
+			if (i < vetorA.length - 1) {
+				System.out.print(vetorA[i] + ", ");
+			} else {
+				System.out.println(vetorA[i] + "]");
+			}
+		}
+		
+		System.out.print("Vetor B = [");
+		for (int i = 0; i < vetorB.length; i++) {
+			if (i < vetorB.length - 1) {
+				System.out.print(vetorB[i] + ", ");
+			} else {
+				System.out.println(vetorB[i] + "]");
+			}
+		}
+	}
+
+}
